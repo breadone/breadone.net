91e51bef2bfe85817dd7dc59fea90dde96c44bd5
pradyun <pradyun.setti@gmail.com>
2023-06-12 19:04:23 +1200

    heathclipped hopefully works somewhat nicer now

--- server/getHeathcliff.py
+++ server/getHeathcliff.py
@@ -2,17 +2,23 @@ import json
 from datetime import datetime as dt
 import urllib.request as req
 
-# hosted on an AWS Lambda instance
-
-def getHeathcliff(event, context):
-    # check is date has been passed in the event, else use today
-    if len(event.keys()) == 0:
+def lambda_handler(event, context):
+    
+    try:
+        date = event['body']['date']
+    except Exception:
         date = dt.today().strftime('%Y/%m/%d')
-        dateComps = date.split("/")
-    else:
-        date = event["date"]
-        dateComps = date.split("/")
-
+    print(event)
+    dateComps = date.split("/")
+    
+    # check is date has been passed in the event, else use today
+    # if ('body' not in event) or ('date' not in event['body']):
+    #     date = dt.today().strftime('%Y/%m/%d')
+    #     dateComps = date.split("/")
+    # else:
+    #     date = event['body']['date']
+    #     dateComps = date.split("/")
+    
     fp = req.urlopen(f"https://www.gocomics.com/heathcliff/{date}")
     mybytes = fp.read()
 
@@ -24,16 +30,20 @@ def getHeathcliff(event, context):
         urlStart = html.index("data-image") + 12
         imageUrl = html[urlStart:].split("\"")[0]
     else:
-        newEvent = { # ⬇️ wtf python
-            "date": '/'.join([dateComps[0], dateComps[1], str(int(dateComps[2])-1)]) # use yesterdays date if not there
+        return {
+            'statusCode': 404,
+            'body': date
         }
-        return getHeathcliff(newEvent, context)
-
-    return {
+        # newDate = '/'.join([dateComps[0], dateComps[1], str(int(dateComps[2])-1)]) # use yesterdays date if not there
+        # newEvent = event
+        # newEvent['body'] = {"date": newDate}
+        # return lambda_handler(newEvent, context)
+    
+    response = {
         'statusCode': 200,
         'body': imageUrl
     }
-
-
-if __name__ == "__main__":
-    print( getHeathcliff({}, {}) )
+    
+    print(response)
+    
+    return response
