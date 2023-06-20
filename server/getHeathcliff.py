20513e2534addebcf13da9f9c31b3db39f8541d0
pradyun <pradyun.setti@gmail.com>
2023-06-20 21:30:17 +1200

    tweaked getHeathcliff again to make it function lo

--- server/getHeathcliff.py
+++ server/getHeathcliff.py
@@ -1,10 +1,12 @@
 from urllib import request as req
-
+import base64
 
 def lambda_handler(event, context):
     # check is date has been passed in the event, else use today
-    if ('body' in event) and ('comicDate' in event['body']):
-        date = event['body']['comicDate']
+    if ('body' in event):
+        b64date = event['body']
+        date = base64.b64decode(b64date).decode("utf8")
+        print("sanity check:", date, type(date))
         return comicWithDate(date)
 
     r = req.Request('https://www.creators.com/read/heathcliff', headers={'User-Agent': 'Mozilla/5.0'})
@@ -23,18 +25,29 @@ def lambda_handler(event, context):
 
 # date is a string, formatted as %Y/%m/%d
 def comicWithDate(date):
-    r = req.Request(f"https://www.gocomics.com/heathcliff/{date}", headers={'User-Agent': 'Mozilla/5.0'})
-    
-    html = req.urlopen(r).read().decode("utf8") 
+    notFoundURL = 'https://cdn.discordapp.com/attachments/760773607550353428/1120629965721960498/heathcliff_notfound.png'
 
-    # check to make sure image exists today
+    html = None
+    comicURL = f"https://www.gocomics.com/heathcliff/{date}"
+
+    r = req.Request(comicURL, headers={'User-Agent': 'Mozilla/5.0'})
+
+    try:
+        html = req.urlopen(r).read().decode("utf8") 
+    except:
+        return {
+            'statusCode': 404,
+            'body': notFoundURL
+        }
+
+    # check to make sure image exists today (it REALLY should given the above block but im being cautious)
     if "data-image" in html:
         urlStart = html.index("data-image") + 12
         imageUrl = html[urlStart:].split("\"")[0]
     else:
         return {
             'statusCode': 404,
-            'body': 'https://cdn.discordapp.com/attachments/760773607550353428/1120629965721960498/heathcliff_notfound.png'
+            'body': notFoundURL
         }
     
     response = {
