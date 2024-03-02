af4dbb7cbf5c8dc8fde53a42bb470321665cf21e
junpier <juniper@breadone.xyz>
2024-03-02 19:46:51 +1300

    Add fediverse link

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -17,8 +17,8 @@ export const MENU_LINKS = [
 	},
 ];
 
-// ! Remember to add your own socials
 export const SOCIAL_LINKS = {
 	email: "bohrium_sugars_0g@icloud.com",
 	github: "https://github.com/breadone",
+	fedi: "https://blahaj.zone/@breadone"
 };
