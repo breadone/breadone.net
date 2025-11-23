b43f49607437b8279c4833cd4cb1e176477fe478
June <self@breadone.net>
2025-11-23 21:32:37 +1300

    Add slug field for albums, sort by publish date rather than creation date, modify date formatting

--- web/src/utils/date.ts
+++ web/src/utils/date.ts
@@ -1,11 +1,9 @@
+const dateFormat = new Intl.DateTimeFormat("en-GB", {day: "numeric", month: "short", year: "numeric"});
 
-export function getFormattedDate(date: string | number | Date) {
-	
-	const dateFormat = new Intl.DateTimeFormat("en-GB", {
-		day: "numeric",
-		month: "short",
-		year: "numeric",
-	});
-	
+export function getFormattedDate(date: string | number | Date, options?: Intl.DateTimeFormatOptions) {
+	if (typeof options !== "undefined") {
+		return new Date(date).toLocaleDateString("en-GB", options);
+	}
+
 	return dateFormat.format(new Date(date));
 }
