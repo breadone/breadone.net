85ad16749c20bdd5920c3aa59ce97ac39165c231
breadone <1@breadone.xyz>
2025-11-23 08:38:50 +0000

    [PB] Update schema

---
+++ pb/pb_migrations/1763886876_updated_albums.js
@@ -0,0 +1,64 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3287366145")
+
+  // remove field
+  collection.fields.removeById("text2862495610")
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2560465762",
+    "max": 0,
+    "min": 0,
+    "name": "slug",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": true,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(3, new Field({
+    "hidden": false,
+    "id": "date2862495610",
+    "max": "",
+    "min": "",
+    "name": "date",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "date"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3287366145")
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2862495610",
+    "max": 0,
+    "min": 0,
+    "name": "date",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // remove field
+  collection.fields.removeById("text2560465762")
+
+  // remove field
+  collection.fields.removeById("date2862495610")
+
+  return app.save(collection)
+})
